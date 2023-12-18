c73276c0c7b2e0adcc56ab90cd361c934dc5a8dd
mhchia <kevin.mh.chia@gmail.com>
2023-12-18 19:49:14 +0800

    feat: support verify and use next

--- next.config.js
+++ next.config.js
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+
+// Ref: https://github.com/zkonduit/ezkljs-engine/tree/main?tab=readme-ov-file#cross-origin-isolation
+const nextConfig = {
+    // webpack: (config) => {
+    //   config.resolve.alias['@ezkljs/verify'] = '@ezkljs/verify/dist/esm/index.js';
+    //   return config;
+    // },
+    async headers() {
+      return [
+        {
+          source: '/(.*)',
+          headers: [
+            {
+              key: 'Cross-Origin-Embedder-Policy',
+              value: 'require-corp',
+            },
+            {
+              key: 'Cross-Origin-Opener-Policy',
+              value: 'same-origin',
+            },
+          ],
+        },
+      ]
+    },
+  }
+
+  module.exports = nextConfig
